bc70997eec8e0f0f6aab83573cfdfa9692bdbfd4
youngslickcrick <christopheriheanacho98@gmail.com>
2025-04-03 17:17:45 -0400

    Update ysc_mediapipe_asl_ros.py

--- ysc_mediapipe_asl_ros.py
+++ ysc_mediapipe_asl_ros.py
@@ -15,14 +15,27 @@ import mediapipe as mp
 import intera_interface
 from collections import deque
 from cv_bridge import CvBridge, CvBridgeError
-#import gripper_opener
-#from gripper_opener import grip
+
+"""
+rospy: Python client library for ROS that enables programmers to communicate with ROS topics, services, and parameters. In other words it uses the ROS python API for communication with Sawyer.
+cv2(openCV): Imports the openCV library which provides functions for and classes for image Image processing and computer vision. It’s essential in opening up sawyer’s cameras.
+os (operating systems): This allows for file manipulation and management, this is needed when saving images and javascript object notation (json) files to a specific directory.
+json(Javascript object notation): Used to store and transfer data, in this code it’s used to save the coordinates of the hand landmarks in JSON format.
+mediapipe: This is used for the hand tracking.
+intera_interface: The python API for communicating with Intera-enabled robots
+cv_bridge: The bridge between ROS image messages and openCV image representation
+time: Is used for handling time based operations it’s used when measuring how long a gesture has been held.
+numpy: Is used to call mathematical equations in multiple functions
+math: Is used for trigonometric and angle calculations.
+deque: Is used for storing past frames, helps improve the accuracy of the gestures being by comparing the last few frames and making sure most of them detect the last gesture.
+"""
 
 
+#Global Variables (variables that can be accessed in and out of a function)
 bridge = CvBridge()
 latest_frame = None
 
-
+#FROM MEDIAPIPE TEMPLATE: Mediapipe initialization
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, 
                        max_num_hands=1,
@@ -31,6 +44,7 @@ hands = mp_hands.Hands(static_image_mode=False,
 mp_drawing = mp.solutions.drawing_utils
 
 
+#Opens and closes the sawyer gripper
 def grip(close=False):
     gripper = intera_interface.Gripper()
     
@@ -40,7 +54,7 @@ def grip(close=False):
         gripper.open()
         
         
-
+#Processes images from Sawyer's head camera
 def camera_callback(img_data, camera_name):
     global latest_frame
     try:
@@ -51,6 +65,7 @@ def camera_callback(img_data, camera_name):
     except CvBridgeError as e:
         rospy.logerr(f"CV Bridge Error: {e}")
 
+#Normalizes landmarks to the wrist landmark
 def normalize_landmarks(landmarks): 
 
     wrist = landmarks[0]  
@@ -64,6 +79,7 @@ def normalize_landmarks(landmarks):
 
     return normalized_landmarks
 
+#Attributes a weighted system to the coordinates
 def weighted_distance(saved, detected):
     weight_x = 1.0 
     weight_y = 1.0  
@@ -76,6 +92,7 @@ def weighted_distance(saved, detected):
         (weight_z * (saved['z'] - detected['z']))**2)
 
 
+#Displays translated gestures on sawyer head screen
 def create_display_image(detected_gesture, word_spelled):
 
     width, height = 1024, 600  
@@ -97,7 +114,7 @@ def create_display_image(detected_gesture, word_spelled):
     return image
 
 
-
+#Compares the saved coordinates of landmarks to the new ones detected by the camera
 def compare_gesture_live(threshold=0.05, hold_time=.5, history_frames=5):
  
     SAVED_GESTURES_PATH = "/home/ysc/ros_ws/src/intera_sdk/intera_examples/scripts/captured_gestures_ros"
@@ -109,7 +126,7 @@ def compare_gesture_live(threshold=0.05, hold_time=.5, history_frames=5):
         if os.path.exists(json_path):
             with open(json_path, 'r') as f:
                 saved_landmarks[label] = normalize_landmarks(json.load(f))
-    
+    #Intializes rospy nodes for running
     rospy.init_node("sawyer_gesture_recognition", anonymous=True)
 
    
@@ -139,33 +156,36 @@ def compare_gesture_live(threshold=0.05, hold_time=.5, history_frames=5):
     word_spelled = []
     limb = intera_interface.Limb()
     
+    #Joint angles for different positions
     C1 = {'right_j0': 0.1262578125, 'right_j1': 0.432787109375, 'right_j2': 0.2639326171875, 'right_j3': -0.4753544921875, 'right_j4': 2.9767138671875, 'right_j5': -1.6180185546875, 'right_j6': -0.989341796875}
     
     O = {'right_j0': 0.0, 'right_j1': 0.0, 'right_j2': 0.0, 'right_j3': -0.0, 'right_j4': 0.0, 'right_j5': 0.0, 'right_j6': -0.0}
 
     C3 = {'right_j0': 0.519818359375, 'right_j1': 0.24602734375, 'right_j2': 0.2218564453125, 'right_j3': -0.1082734375, 'right_j4': 2.9670185546875, 'right_j5': -1.3883837890625, 'right_j6': -0.93182421875}
 
-    
+
+
+    #Keeps the function looping as long ROS is active
     while not rospy.is_shutdown():
         if latest_frame is None:
             continue
 
         frame = latest_frame.copy()
 
-        
+        #FROM MEDIAPIPE TEMPLATE: Used to convert frame to RGB which mediapipe runs in
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = hands.process(rgb_frame)
 
         gesture_matched = False  
 
-
+        #Creates a green box on the camera feed screen
         height, width = frame.shape[:2]
         box_size = int(height * 0.2)
         top_left = (width // 2 - box_size // 2, int(height * 0.25))  
         bottom_right = (top_left[0] + box_size, top_left[1] + box_size)
         cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0), 2)
 
-
+        #FROM MEDIAPIPE TEMPLATE: Draws the landmarks on the hand
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
               
@@ -174,31 +194,33 @@ def compare_gesture_live(threshold=0.05, hold_time=.5, history_frames=5):
                     mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=4),
                     mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2))
 
-
+                #Saves detected landmarks as a dictonary and normalizes them
                 detected_landmarks = [{'x': lm.x, 'y': lm.y, 'z': lm.z} for lm in hand_landmarks.landmark]
-                detected_landmarks = normalize_landmarks(detected_landmarks)    
+                detected_landmarks = normalize_landmarks(detected_landmarks)   
+                 
+                 
+                #Loops through letters till it finds a match that meets threshold
                 for label, landmarks in saved_landmarks.items():
                     match = True
                     for saved, detected in zip(landmarks, detected_landmarks):
-                    
                         if weighted_distance(saved, detected) > threshold: 
-                      
                             match = False
                             break
                     
+                    #Adds latest detected letter to recent_dectections
                     if match:
                         gesture_matched = True  
                         recent_detections.append(label)  
                         
-                        
+                        #Determines which dectections shows up the most frequently in recent_detections
                         most_common = max(set(recent_detections), key=recent_detections.count) 
                         if recent_detections.count(most_common) >= history_frames * 0.6:  
                             detected_gesture = most_common
                         else:
                             detected_gesture = None
                            
+                        #Starts hold time for hand gesture   
                         if detected_gesture and detected_gesture == label:
-
                             if gesture_start_time is None:
                                 gesture_start_time = time.time()
                             elif time.time() - gesture_start_time >= hold_time:
@@ -206,7 +228,7 @@ def compare_gesture_live(threshold=0.05, hold_time=.5, history_frames=5):
                                 print(f"Gesture confirmed: {label}")
                                 gesture_start_time = None  
                                 
-                                                        
+                                #Confirms spelled word                        
                                 if label == "finish":
                                     print(f"Word Confirmed: {''.join(word_spelled)}")
                                     sword = ''.join(word_spelled)
@@ -226,36 +248,40 @@ def compare_gesture_live(threshold=0.05, hold_time=.5, history_frames=5):
                                     elif sword == 'CL':
                                         grip(close=True)    
                                         
-                                        
+                                    #After word is confirmed, new the list is emptied  
                                     word_spelled = []
 
 
-                                    
+                                #Deletes last letter  
                                 elif label == "backspace":
                                     if word_spelled:
                                         word_spelled.pop()  
                                         print(f"Word after backspace: {''.join(word_spelled)}")
+                                
+                                #Combines latest letter to last letter        
                                 else:
                                     word_spelled.append(label)  
                                     print(f"Current word: {''.join(word_spelled)}")
                                     
                                     if word_spelled == 'B':
                                         print(f"Word Confirmed: {''.join(word_spelled)}")
-
+                        #Resets the loop
                         else:                         
                             detected_gesture = label
                             gesture_start_time = time.time()
                         break
+                        
                 else:
-                   
                     detected_gesture = None
                     gesture_start_time = None
-
+                    
+                    
+                    
+        #Prints confirmed and detected letter on camera feed 
         if gesture_matched:
             text = f"Detected: {confirmed_gesture if confirmed_gesture == label and detected_gesture == label else detected_gesture}"
             if confirmed_gesture == label and detected_gesture == label:
                 color = (0, 255, 0)
-               
             else:
                 color = (0, 255, 255)
 
@@ -264,21 +290,21 @@ def compare_gesture_live(threshold=0.05, hold_time=.5, history_frames=5):
             confirmed_gesture = None  
 
 
-
+        #Opens the camera feed
         cv2.imshow("Sawyer Head Camera - Gesture Recognition", frame)
         
-
+        #Displays image to sawyer head screen
         display_img = create_display_image(detected_gesture, word_spelled)
         temp_image_path = "/tmp/sawyer_display.png"
         cv2.imwrite(temp_image_path, display_img)  
         head_display.display_image(temp_image_path, display_in_loop=False, display_rate=10.0)        
         
+        #Closes script if '.' is pressed
         if cv2.waitKey(1) & 0xFF == 46:
             break
             
-        #head_display.display_image('/home/ysc/Pictures/Default_Image.png', display_in_loop=True, display_rate=1)
     cv2.destroyAllWindows()
 
-
+#Used as guard for against other scripts allowing it to run properly if called
 if __name__ == "__main__":
     compare_gesture_live(threshold=0.045, hold_time=1.2, history_frames=2) 
